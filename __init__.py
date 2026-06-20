4dbad13b2550d75c1c352b50a9360f907d08fe6d
XINGGUANG YAN <87305660+xingguangYan@users.noreply.github.com>
2026-06-20 15:31:49 +0800

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+GeoPilot - AI-powered Geospatial Analysis Assistant for QGIS
+"""
+import os, sys
+
+def classFactory(iface):
+    from geopilot import GeoPilotPlugin
+    return GeoPilotPlugin(iface)
